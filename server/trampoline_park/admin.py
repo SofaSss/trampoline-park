f80fe8012aa9409b92a462a5be0be4877843d5bf
SofaSss <ssvideo.kontent@gmail.com>
2025-03-14 13:22:59 +0300

    ксатомизация, локализация админ-панели: для красивого оформления прописаны классы CustomAdmin, для понятного отображения полей в моделять дописаны class Meta, def __str__(self)

--- server/trampoline_park/admin.py
+++ server/trampoline_park/admin.py
@@ -20,11 +20,11 @@ class CustomUserAdmin(UserAdmin):
 
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "phone_number", "is_healthy", "user")
-    search_fields = ("first_name", "last_name", "phone_number")
-    list_filter = ("is_healthy",)
+    list_display = ('first_name', 'last_name', 'phone_number', 'is_healthy', 'user')
+    search_fields = ('first_name', 'last_name', 'phone_number')
+    list_filter = ('is_healthy',)
     readonly_fields = (
-        "first_name", "last_name", "date_of_birth", "phone_number", "profile_picture", "is_healthy", "user")
+        'first_name', 'last_name', 'date_of_birth', 'phone_number', 'profile_picture', 'is_healthy', 'user')
 
     def has_change_permission(self, request, obj=None):
         return False
@@ -61,15 +61,87 @@ class CoachAdmin(admin.ModelAdmin):
     ordering = ('last_name', 'first_name')
 
 
+class CoachSpecialtyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class CoachAchievementAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class WorkoutTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'duration', 'max_clients')
+    search_fields = ('name',)
+
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('datetime', 'workout_type', 'coach')
+    search_fields = ('coach__last_name', 'coach__first_name', 'clients__first_name', 'clients__last_name')
+
+    fieldsets = (
+        ('Тренер', {
+            'fields': ('coach',)
+        }),
+        ('Клиент(ы)', {
+            'fields': ('clients',)
+        }),
+        ('Дата', {
+            'fields': ('datetime',)
+        }),
+        ('Детали', {
+            'fields': ('workout_type',)
+        }),
+    )
+
+
+class TypeOptionalServiceAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class OptionalServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'price')
+    search_fields = ('name',)
+
+
+class CoachCostumeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'clients', 'coach', 'coach_costume')
+    search_fields = ('name', 'clients__first_name', 'clients__last_name', 'coach__first_name', 'coach__last_name')
+    fieldsets = (
+        ('Информация о заказчике', {
+            'fields': ('clients', 'name',)
+        }),
+        ('Дата', {
+            'fields': ('date', 'event_start_time', 'event_end_time',)
+        }),
+        ('Аниматор', {
+            'fields': ('coach', 'coach_costume',)
+        }),
+        ('Детали', {
+            'fields': ('is_photographer', 'is_videographer', 'optional_service',)
+        }),
+    )
+
+
+admin.site.site_header = "Администрирование Батутного парка №1"
+admin.site.site_title = "Батутный парк №1 | Панель администратора"
+admin.site.index_title = "Добро пожаловать в админ-панель"
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Coach, CoachAdmin)
-admin.site.register(CoachSpecialty)
-admin.site.register(CoachAchievement)
-admin.site.register(WorkoutType)
-admin.site.register(Workout)
-admin.site.register(TypeOptionalService)
-admin.site.register(OptionalService)
-admin.site.register(CoachCostume)
-admin.site.register(Event)
+admin.site.register(CoachSpecialty, CoachSpecialtyAdmin)
+admin.site.register(CoachAchievement, CoachAchievementAdmin)
+admin.site.register(WorkoutType, WorkoutTypeAdmin)
+admin.site.register(Workout, WorkoutAdmin)
+admin.site.register(TypeOptionalService, TypeOptionalServiceAdmin)
+admin.site.register(OptionalService, OptionalServiceAdmin)
+admin.site.register(CoachCostume, CoachCostumeAdmin)
+admin.site.register(Event, EventAdmin)
 admin.site.register(PhotoVideoServicePrice)
--- server/trampoline_park/models.py
+++ server/trampoline_park/models.py
@@ -16,25 +16,51 @@ class User(AbstractUser):
     EMAIL_FIELD = "email"  # для автоматической отправки писем
     USERNAME_FIELD = "username"  # login для авторизации обязательное поле
     REQUIRED_FIELDS = []  # больше обязательных полей не будет
-    role = models.CharField(max_length=50, choices=ROLES, default="ADMIN")
+    role = models.CharField(max_length=50, choices=ROLES, default="ADMIN", verbose_name="Роль пользователя")
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
 
 class Client(models.Model):
     first_name = models.CharField(max_length=50, blank=False, null=False, verbose_name="Имя")
     last_name = models.CharField(max_length=50, blank=False, null=False, verbose_name="Фамилия")
     date_of_birth = models.DateField(blank=False, null=False, verbose_name="Дата рождения")
-    phone_number = models.CharField(max_length=20, blank=False, null=False,verbose_name="Номер телефона" )
-    profile_picture = models.FileField(upload_to="profile_pictures", null=True, blank=True, verbose_name="Фотография профиля")
+    phone_number = models.CharField(max_length=20, blank=False, null=False, verbose_name="Номер телефона")
+    profile_picture = models.FileField(upload_to="profile_pictures", null=True, blank=True,
+                                       verbose_name="Фотография профиля")
     is_healthy = models.BooleanField(verbose_name="Сведения о здорвье")
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class CoachSpecialty(models.Model):
     name = models.CharField(max_length=150, verbose_name="Специальность")
 
+    class Meta:
+        verbose_name = "Специальность тренера"
+        verbose_name_plural = "Специальности тренера"
+
+    def __str__(self):
+        return self.name
+
 
 class CoachAchievement(models.Model):
     name = models.CharField(max_length=150, verbose_name="Достижение")
+
+    class Meta:
+        verbose_name = "Достижение тренера"
+        verbose_name_plural = "Достижения тренера"
+
+    def __str__(self):
+        return self.name
 
 
 class Coach(models.Model):
@@ -49,6 +75,13 @@ class Coach(models.Model):
     specialties = models.ManyToManyField(CoachSpecialty, verbose_name="Специальности")
     achievements = models.ManyToManyField(CoachAchievement, verbose_name="Достижения")
 
+    class Meta:
+        verbose_name = "Тренер"
+        verbose_name_plural = "Тренеры"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class WorkoutType(models.Model):
     name = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name="Название тренировки")
@@ -58,16 +91,38 @@ class WorkoutType(models.Model):
     duration = models.IntegerField(verbose_name="Продолжительность")
     max_clients = models.IntegerField(default=1, verbose_name="Максимальное количество клиентов")
 
+    class Meta:
+        verbose_name = "Тип тренировки"
+        verbose_name_plural = "Типы тренировок"
+
+    def __str__(self):
+        return self.name
+
 
 class Workout(models.Model):
     datetime = models.DateTimeField(null=False, blank=False, verbose_name="Дата и время")
-    workout_type = models.ForeignKey(WorkoutType, on_delete=models.CASCADE, null=False, blank=False, verbose_name="Тип тренировки")
+    workout_type = models.ForeignKey(WorkoutType, on_delete=models.CASCADE, null=False, blank=False,
+                                     verbose_name="Тип тренировки")
     coach = models.ForeignKey(Coach, on_delete=models.CASCADE, null=False, blank=False, verbose_name="Тренер")
-    clients = models.ManyToManyField(Client, blank=True,verbose_name="Клиент(ы)")
+    clients = models.ManyToManyField(Client, blank=True, verbose_name="Клиент(ы)")
+
+    class Meta:
+        verbose_name = "Тренировка"
+        verbose_name_plural = "Тренировки"
+
+    def __str__(self):
+        return f"{self.workout_type} - {self.datetime.strftime('%d.%m.%Y %H:%M')} - {self.coach}"
 
 
 class TypeOptionalService(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="Тип дополнительной услуги")
+
+    class Meta:
+        verbose_name = "Тип дополнительной услуги"
+        verbose_name_plural = "Типы дополнительных услуг"
+
+    def __str__(self):
+        return self.name
 
 
 class OptionalService(models.Model):
@@ -76,15 +131,36 @@ class OptionalService(models.Model):
     type = models.ForeignKey(TypeOptionalService, on_delete=models.CASCADE, verbose_name="Тип услуги")
     price = models.IntegerField(verbose_name="Цена")
 
+    class Meta:
+        verbose_name = "Дополнительная услуга"
+        verbose_name_plural = "Дополнительные услуги"
+
+    def __str__(self):
+        return self.name
+
 
 class CoachCostume(models.Model):
     name = models.CharField(max_length=100, unique=True, blank=False, null=False, verbose_name="Название")
     price = models.IntegerField(verbose_name="Цена")
 
+    class Meta:
+        verbose_name = "Костюм тренера"
+        verbose_name_plural = "Костюмы тренера"
+
+    def __str__(self):
+        return self.name
+
 
 class PhotoVideoServicePrice(models.Model):
     photographer_price = models.IntegerField(blank=False, null=False, verbose_name="Цена фотографа")
     videographer_price = models.IntegerField(blank=False, null=False, verbose_name="Цена видеографа")
+
+    class Meta:
+        verbose_name = "Цена фото/видео услуг"
+        verbose_name_plural = "Цены фото/видео услуг"
+
+    def __str__(self):
+        return f"Фотограф: {self.photographer_price} ₽, Видеограф: {self.videographer_price} ₽"
 
 
 class Event(models.Model):
@@ -93,8 +169,16 @@ class Event(models.Model):
     event_start_time = models.TimeField(blank=False, null=False, verbose_name="Время начала")
     event_end_time = models.TimeField(blank=False, null=False, verbose_name="Время конца")
     is_photographer = models.BooleanField(blank=False, null=False, verbose_name="Услуги фотографа")
-    is_videographer = models.BooleanField(blank=False, null=False,verbose_name="Услуги видеографа")
-    optional_service = models.ForeignKey(OptionalService, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Дополнительные услуги")
+    is_videographer = models.BooleanField(blank=False, null=False, verbose_name="Услуги видеографа")
+    optional_service = models.ForeignKey(OptionalService, on_delete=models.CASCADE, null=True, blank=True,
+                                         verbose_name="Дополнительные услуги")
     coach = models.ForeignKey(Coach, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Тренер")
     coach_costume = models.ForeignKey(CoachCostume, on_delete=models.CASCADE, verbose_name="Костюм")
     clients = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name="Клиент")
+
+    class Meta:
+        verbose_name = "Мероприятие"
+        verbose_name_plural = "Мероприятия"
+
+    def __str__(self):
+        return f"{self.name} - {self.date.strftime('%d.%m.%Y')} - {self.clients}"
