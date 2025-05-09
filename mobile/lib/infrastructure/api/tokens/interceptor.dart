f87003eecda95c490212839f68a088941d999215
SofaSss <ssvideo.kontent@gmail.com>
2025-05-09 21:04:55 +0300

    исправление переноса на начальный экран при истечении рефреш токена

--- mobile/lib/infrastructure/api/tokens/interceptor.dart
+++ mobile/lib/infrastructure/api/tokens/interceptor.dart
@@ -3,20 +3,21 @@ part of '../../infrastructure_part.dart';
 class DioInterceptor extends Interceptor {
   final TokenUseCases tokenUseCase;
 
+  final excludePaths = [
+    'client/create/',
+    'auth/jwt/create/',
+    'auth/jwt/refresh/',
+    'auth/users/activation/',
+    'auth/users/reset_password/',
+    'auth/users/reset_password_confirm/',
+  ];
+
   DioInterceptor(this.tokenUseCase);
   @override
   Future<void> onRequest(
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
-    final excludePaths = [
-      'client/create/',
-      'auth/jwt/create/',
-      'auth/jwt/refresh/',
-      'auth/users/activation/',
-      'auth/users/reset_password/',
-      'auth/users/reset_password_confirm/',
-    ];
     final isUnSignInPath = excludePaths.any((p) => (options.path.contains(p)));
     if (isUnSignInPath) {
       return handler.next(options);
@@ -35,6 +36,12 @@ class DioInterceptor extends Interceptor {
     final options = err.requestOptions;
 
     if (err.response?.statusCode == 401) {
+      final isUnSignInPath = excludePaths.any(
+        (p) => (options.path.contains(p)),
+      );
+      if (isUnSignInPath) {
+        return handler.reject(err);
+      }
       try {
         await tokenUseCase.deleteAccessToken();
         final newTokens = await tokenUseCase.refreshTokens();
