d136f2fb787a189c7163663c72a3dbfec280bdf3
SofaSss <ssvideo.kontent@gmail.com>
2025-05-11 21:05:42 +0300

    отдача медиа файлов при выключенном дебаге

--- server/trampoline_park_api/settings.py
+++ server/trampoline_park_api/settings.py
@@ -106,12 +106,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-        'LOCATION': os.path.join(BASE_DIR, 'media'),
     },
-    'default': {
-        'BACKEND': 'django.core.files.storage.FileSystemStorage',
-        'LOCATION': os.path.join(BASE_DIR, 'media'),
-    },
+
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
